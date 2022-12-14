72b89955fc22eca35eb3f82f4b76dc8cfc8d04aa
tora.yeh <tora.yeh@thinktronltd.com>
2022-12-14 19:50:00 +0800

    調整單引號與雙引號

--- vue/src/layouts/MainLayout.vue
+++ vue/src/layouts/MainLayout.vue
@@ -1,34 +1,34 @@
 <template >
   <q-list class="lg:(px-12) md:(px-2)" bordered>
-    <q-item-label class='text-2xl text-center text-bold p-3'>新北市政府農業局違規使用山坡地案件現場會勘紀錄表</q-item-label>
+    <q-item-label class="text-2xl text-center text-bold p-3">新北市政府農業局違規使用山坡地案件現場會勘紀錄表</q-item-label>
     <q-item>
       <q-item-section side>
-        <q-icon name='article' />
+        <q-icon name="article" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>案由: </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>案由: </q-item-section>
       <q-item-section>
-        <q-input v-model='inspectRecord["案由"]' filled dense />
+        <q-input v-model="inspectRecord['案由']" filled dense />
       </q-item-section>
     </q-item>
     <q-item>
       <q-item-section side>
-        <q-icon name='event_available' />
+        <q-icon name="event_available" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>時間: </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>時間: </q-item-section>
       <q-item-section>
         <q-input readonly filled dense>
           <template v-slot:append>
-            <q-icon name='event' class='cursor-pointer'>
-              <q-popup-proxy cover transition-show='scale' transition-hide='scale'>
-                <div class='p-3'>
-                  <div class='q-gutter-md row items-start'>
-                    <q-date v-model='inspectRecord["時間"]' mask='YYYY-MM-DD HH:mm' />
-                    <q-time v-model='inspectRecord["時間"]' mask='YYYY-MM-DD HH:mm' />
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                <div class="p-3">
+                  <div class="q-gutter-md row items-start">
+                    <q-date v-model="inspectRecord['時間']" mask="YYYY-MM-DD HH:mm" />
+                    <q-time v-model="inspectRecord['時間']" mask="YYYY-MM-DD HH:mm" />
                   </div>
-                  <div class='pt-3'>
-                    <div class='row items-center justify-end my-2'>
-                      <q-btn v-close-popup label='關閉' color='grey' flat />
-                      <q-btn v-close-popup label='確定' color='positive' flat />
+                  <div class="pt-3">
+                    <div class="row items-center justify-end my-2">
+                      <q-btn v-close-popup label="關閉" color="grey" flat />
+                      <q-btn v-close-popup label="確定" color="positive" flat />
                     </div>
                   </div>
                 </div>
@@ -38,21 +38,21 @@
         </q-input>
       </q-item-section>
     </q-item>
-    <q-expansion-item class='text-lg text-bold' expand-separator icon='supervisor_account' label='會勘單位與人員'>
+    <q-expansion-item class="text-lg text-bold" expand-separator icon="supervisor_account" label="會勘單位與人員">
       <q-card>
         <q-card-section>
-          <q-item v-for='_attendees, n of attendees' :key='n'>
-            <q-item-section class='text-lg text-black text-bold' side>{{ _attendees.name }}</q-item-section>
+          <q-item v-for="_attendees, n of attendees" :key="n">
+            <q-item-section class="text-lg text-black text-bold" side>{{ _attendees.name }}</q-item-section>
             <q-item-section>
-              <q-select v-show="(_attendees.type === 'select')" v-model='inspectRecord[_attendees.name]' :options='_attendees.data' :label=_attendees.label filled dense />
-              <q-input v-show="(_attendees.type === 'input')" v-model='inspectRecord[_attendees.name]' :label=_attendees.label filled dense />
+              <q-select v-show="(_attendees.type === 'select')" v-model="inspectRecord[_attendees.name]" :options="_attendees.data" :label=_attendees.label filled dense />
+              <q-input v-show="(_attendees.type === 'input')" v-model="inspectRecord[_attendees.name]" :label=_attendees.label filled dense />
             </q-item-section>
             <q-item-section side>
-              <q-btn icon='edit' color='grey-3' class='text-grey-7' @click='isShowDialog.district = true' dense />
-              <q-dialog v-model='isShowDialog.district'>
-                <q-card class='w-50vw h-50vh'>
-                  <q-card-section class='flex justify-end p-2'>
-                    <q-btn icon='close' flat round dense v-close-popup />
+              <q-btn icon="edit" color="grey-3" class="text-grey-7" @click="isShowDialog.district = true" dense />
+              <q-dialog v-model="isShowDialog.district">
+                <q-card class="w-50vw h-50vh">
+                  <q-card-section class="flex justify-end p-2">
+                    <q-btn icon="close" flat round dense v-close-popup />
                   </q-card-section>
                   <q-card-section>
                     <p>請簽名於此處</p>
@@ -64,39 +64,39 @@
         </q-card-section>
       </q-card>
     </q-expansion-item>
-    <q-expansion-item class='text-lg text-bold' expand-separator icon='place' label='土地基本資料'>
+    <q-expansion-item class="text-lg text-bold" expand-separator icon="place" label="土地基本資料">
       <q-card>
         <q-card-section>
-      <q-item>
-      <q-item-section class='text-lg text-black text-bold' side>衛星定位座標(TWD97) </q-item-section>
-      <q-item-section side>X座標</q-item-section>
-      <q-item-section>
-        <q-input v-model='inspectRecord["TWD97_X"]' label="請輸入X座標" filled dense />
-      </q-item-section>
-      <q-item-section side>Y座標</q-item-section>
-      <q-item-section>
-        <q-input v-model='inspectRecord["TWD97_Y"]' label="請輸入Y座標" filled dense />
-      </q-item-section>
-    </q-item>
-          <q-item v-for='_info, i of info' :key='i'>
-            <q-item-section class='text-lg text-black text-bold' side>{{ _info.name }}</q-item-section>
+          <q-item>
+            <q-item-section class="text-lg text-black text-bold" side>衛星定位座標(TWD97) </q-item-section>
+            <q-item-section side>X座標</q-item-section>
             <q-item-section>
-              <q-select v-show="(_info.type === 'select')" v-model='inspectRecord[_info.name]' :options='_info.data' :label=_info.label filled dense/>
-              <q-select v-show="(_info.type === 'select_district')" v-model='inspectRecord[_info.name]' @update:model-value="getSectionList()" :options='_info.data' :label=_info.label filled dense/>
-              <q-input v-show="(_info.type === 'input')" v-model='inspectRecord[_info.name]' :label=_info.label filled dense />
+              <q-input v-model="inspectRecord['TWD97_X']" label="請輸入X座標" filled dense />
+            </q-item-section>
+            <q-item-section side>Y座標</q-item-section>
+            <q-item-section>
+              <q-input v-model="inspectRecord['TWD97_Y']" label="請輸入Y座標" filled dense />
+            </q-item-section>
+          </q-item>
+          <q-item v-for="_info, i of info" :key="i">
+            <q-item-section class="text-lg text-black text-bold" side>{{ _info.name }}</q-item-section>
+            <q-item-section>
+              <q-select v-show="(_info.type === 'select')" v-model="inspectRecord[_info.name]" :options="_info.data" :label=_info.label filled dense/>
+              <q-select v-show="(_info.type === 'select_district')" v-model="inspectRecord[_info.name]" @update:model-value="getSectionList()" :options="_info.data" :label=_info.label filled dense/>
+              <q-input v-show="(_info.type === 'input')" v-model="inspectRecord[_info.name]" :label=_info.label filled dense />
             </q-item-section>
           </q-item>
         </q-card-section>
       </q-card>
     </q-expansion-item>
-    <q-expansion-item class='text-lg text-bold' expand-separator icon='perm_identity' label='行為人基本資料'>
+    <q-expansion-item class="text-lg text-bold" expand-separator icon="perm_identity" label="行為人基本資料">
       <q-card>
         <q-card-section>
-          <q-item v-for='_infoPerson, i of infoPerson' :key='i'>
-            <q-item-section class='text-lg text-black text-bold' side>{{ _infoPerson.name }}</q-item-section>
+          <q-item v-for="_infoPerson, i of infoPerson" :key="i">
+            <q-item-section class="text-lg text-black text-bold" side>{{ _infoPerson.name }}</q-item-section>
             <q-item-section>
-              <q-select v-show="(_infoPerson.type === 'select')" v-model='inspectRecord[_infoPerson.name]' :options='_infoPerson.data' :label=_infoPerson.label filled dense/>
-              <q-input v-show="(_infoPerson.type === 'input')" v-model='inspectRecord[_infoPerson.name]' :label=_infoPerson.label filled dense />
+              <q-select v-show="(_infoPerson.type === 'select')" v-model="inspectRecord[_infoPerson.name]" :options="_infoPerson.data" :label=_infoPerson.label filled dense/>
+              <q-input v-show="(_infoPerson.type === 'input')" v-model="inspectRecord[_infoPerson.name]" :label=_infoPerson.label filled dense />
             </q-item-section>
           </q-item>
         </q-card-section>
@@ -104,82 +104,82 @@
     </q-expansion-item>
     <q-item>
       <q-item-section side>
-        <q-icon name='info' />
+        <q-icon name="info" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>違規類別(可複選): </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>違規類別(可複選): </q-item-section>
       <q-item-section>
-        <q-select v-model='inspectRecord["違規項目"]' :options='violationType' label="請選擇違規項目" filled dense />
+        <q-select v-model="inspectRecord['違規項目']" :options="violationType" label="請選擇違規項目" filled dense />
       </q-item-section>
       <q-item-section>
-        <q-input v-model='inspectRecord["其他違規項目"]' label="請輸入其他違規項目" filled dense />
+        <q-input v-model="inspectRecord['其他違規項目']" label="請輸入其他違規項目" filled dense />
       </q-item-section>
     </q-item>
     <q-item>
       <q-item-section side>
-        <q-icon name='info_outline' />
+        <q-icon name="info_outline" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>輔導類別: </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>輔導類別: </q-item-section>
       <q-item-section>
         <q-toggle v-model="inspectRecord[guidance]" checked-icon="check" color="green" label="移請水土保持服務團指導實施水土保持處理與維護" unchecked-icon="clear"/>
       </q-item-section>
     </q-item>
     <q-item>
       <q-item-section side>
-        <q-icon name='edit' />
+        <q-icon name="edit" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>各單位意見: </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>各單位意見: </q-item-section>
       <q-item-section>
         <q-input v-model="inspectRecord[各單位意見]" type="textarea" filled dense/>
       </q-item-section>
     </q-item>
     <q-item>
       <q-item-section side>
-        <q-icon name='edit' />
+        <q-icon name="edit" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>請行為人陳述意見: </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>請行為人陳述意見: </q-item-section>
       <q-item-section>
         <q-input v-model="inspectRecord[行為人意見]" type="textarea" filled dense/>
       </q-item-section>
     </q-item>
     <q-item>
       <q-item-section side>
-        <q-icon name='description' />
+        <q-icon name="description" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>會勘結論(可複選): </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>會勘結論(可複選): </q-item-section>
       <q-item-section>
-        <q-select v-model='inspectRecord["會勘結論"]' :options='conclusionType' label="請選擇會勘結論項目" filled dense />
+        <q-select v-model="inspectRecord['會勘結論']" :options="conclusionType" label="請選擇會勘結論項目" filled dense />
       </q-item-section>
       <q-item-section>
-        <q-input v-model='inspectRecord["其他會勘結論"]' label="請輸入其他會勘結論" filled dense />
+        <q-input v-model="inspectRecord['其他會勘結論']" label="請輸入其他會勘結論" filled dense />
       </q-item-section>
     </q-item>
     <q-item>
       <q-item-section side>
-        <q-icon name='collections' />
+        <q-icon name="collections" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>現場照片: </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>現場照片: </q-item-section>
       <q-item-section>
         <q-input @update:model-value="val => { files = val }" multiple type="file" hint="可選擇多個檔案" filled dense/>
       </q-item-section>
     </q-item>
     <q-item>
       <q-item-section side>
-        <q-icon name='watch_later' />
+        <q-icon name="watch_later" />
       </q-item-section>
-      <q-item-section class='text-lg text-black text-bold' side>散會: </q-item-section>
+      <q-item-section class="text-lg text-black text-bold" side>散會: </q-item-section>
       <q-item-section>
         <q-input readonly filled dense>
           <template v-slot:append>
-            <q-icon name='event' class='cursor-pointer'>
-              <q-popup-proxy cover transition-show='scale' transition-hide='scale'>
-                <div class='p-3'>
-                  <div class='q-gutter-md row items-start'>
-                    <q-time v-model='inspectRecord["時間"]' mask='HH:mm' />
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                <div class="p-3">
+                  <div class="q-gutter-md row items-start">
+                    <q-time v-model="inspectRecord['時間']" mask="HH:mm" />
                   </div>
-                  <div class='pt-3'>
-                    <div class='row items-center justify-end my-2'>
-                      <q-btn v-close-popup label='關閉' color='grey' flat />
-                      <q-btn v-close-popup label='確定' color='positive' flat />
+                  <div class="pt-3">
+                    <div class="row items-center justify-end my-2">
+                      <q-btn v-close-popup label="關閉" color="grey" flat />
+                      <q-btn v-close-popup label="確定" color="positive" flat />
                     </div>
                   </div>
                 </div>
@@ -211,7 +211,6 @@ import axios from 'axios'
 import { info, infoPerson, attendees, violation, conclusion } from './data'
 import { useRouter } from 'vue-router'
 import SmoothSignature from 'smooth-signature'
-
 
 let districtData
 async function getVillage () {
