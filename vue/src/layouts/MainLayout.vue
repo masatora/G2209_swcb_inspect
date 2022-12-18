3b3ca5cf76b214d1b4d5c5725a1ffdbb8b80b692
masatora <omadedou@gmail.com>
2022-12-19 07:21:40 +0800

    ui 調整

--- vue/src/layouts/MainLayout.vue
+++ vue/src/layouts/MainLayout.vue
@@ -3,41 +3,45 @@
     <q-list class="h-full lg:px-100 md:px-20 overflow-y-auto" bordered>
       <q-item-label class="text-2xl text-center text-bold p-3">新北市政府農業局違規使用山坡地案件現場會勘紀錄表</q-item-label>
       <q-item>
-        <q-item-section side>
-          <q-icon name="article" />
-        </q-item-section>
-        <q-item-section class="text-lg text-black text-bold" side>案由: </q-item-section>
-        <q-item-section>
-          <q-input v-model="inspectRecord['案由']" filled dense />
-        </q-item-section>
-      </q-item>
-      <q-item>
-        <q-item-section side>
-          <q-icon name="event_available" />
-        </q-item-section>
-        <q-item-section class="text-lg text-black text-bold" side>時間: </q-item-section>
-        <q-item-section>
-          <q-input v-model="inspectRecord['時間']" readonly filled dense>
-            <template v-slot:append>
-              <q-icon name="event" class="cursor-pointer">
-                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                  <div class="p-3">
-                    <div class="q-gutter-md row items-start">
-                      <q-date v-model="inspectRecord['時間']" mask="YYYY-MM-DD HH:mm" />
-                      <q-time v-model="inspectRecord['時間']" mask="YYYY-MM-DD HH:mm" />
-                    </div>
-                    <div class="pt-3">
-                      <div class="row items-center justify-end my-2">
-                        <q-btn v-close-popup label="關閉" color="grey" flat />
-                        <q-btn v-close-popup label="確定" color="positive" flat />
+        <div class="w-full">
+          <div class="grid grid-cols-[0.1fr,0.5fr,1.6fr] gap-5 flex items-center py-2">
+            <span>
+              <q-icon name="article" color="grey-7" size="sm" />
+            </span>
+            <span class="text-lg text-black text-bold">案由: </span>
+            <span>
+              <q-input v-model="inspectRecord['案由']" filled dense />
+            </span>
+          </div>
+          <div class="grid grid-cols-[0.1fr,0.5fr,1.6fr] gap-5 flex items-center py-2">
+            <span>
+              <q-icon name="event" color="grey-7" size="sm" />
+            </span>
+            <span class="text-lg text-black text-bold">時間: </span>
+            <span>
+              <q-input v-model="inspectRecord['時間']" readonly filled dense>
+                <template v-slot:append>
+                  <q-icon name="event" class="cursor-pointer">
+                    <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                      <div class="p-3">
+                        <div class="q-gutter-md row items-start">
+                          <q-date v-model="inspectRecord['時間']" mask="YYYY-MM-DD HH:mm" />
+                          <q-time v-model="inspectRecord['時間']" mask="YYYY-MM-DD HH:mm" />
+                        </div>
+                        <div class="pt-3">
+                          <div class="row items-center justify-end my-2">
+                            <q-btn v-close-popup label="關閉" color="grey" flat />
+                            <q-btn v-close-popup label="確定" color="positive" flat />
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </q-input>
-        </q-item-section>
+                    </q-popup-proxy>
+                  </q-icon>
+                </template>
+              </q-input>
+            </span>
+          </div>
+        </div>
       </q-item>
       <q-expansion-item class="text-lg text-bold my-2 px-4" header-class="bg-blue-1" expand-separator icon="supervisor_account" label="會勘單位與人員">
         <q-card>
